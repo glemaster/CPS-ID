6edd3cd210ab7915b8891fcb1ad2d1789880b881
John Wehrenberg <John@johns-macbook-air.local>
2017-07-10 14:34:31 -0500

    RegistrationInfo NSObject Created

---
+++ CPS ID/RegistrationInfo.swift
@@ -0,0 +1,13 @@
+//
+//  RegistrationInfo.swift
+//  CPS ID
+//
+//  Created by John Wehrenberg on 7/10/17.
+//  Copyright © 2017 Gwyneth Lemaster. All rights reserved.
+//
+
+import UIKit
+
+class RegistrationInfo: NSObject {
+
+}
