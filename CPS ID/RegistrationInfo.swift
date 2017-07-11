7eb6dadf9ade88747b784bc68eefd0ba3e6f262a
John Wehrenberg <John@johns-macbook-air.local>
2017-07-10 22:02:05 -0500

    Fixed bug with data transfer

--- CPS ID/RegistrationInfo.swift
+++ CPS ID/RegistrationInfo.swift
@@ -15,8 +15,7 @@ class registrationInfo: NSObject {
     var lunchPeriod = Int()
     var idNum = Int()
     var school = String()
-    var profilePic = Data()
-    
+    var profilePic = UIImage()
     
     
 
