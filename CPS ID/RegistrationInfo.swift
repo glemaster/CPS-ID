d03df3327baa47eef29b12325daf7334439d8f6c
Huangerb <bradley.c.yu@gmail.com>
2017-07-11 09:25:19 -0500

    fixed registration info

--- CPS ID/RegistrationInfo.swift
+++ CPS ID/RegistrationInfo.swift
@@ -2,19 +2,21 @@
 //  RegistrationInfo.swift
 //  CPS ID
 //
-//  Created by John Wehrenberg on 7/10/17.
+//  Created by Bradley Yu on 7/11/17.
 //  Copyright © 2017 Gwyneth Lemaster. All rights reserved.
 //
 
+import Foundation
 import UIKit
 
-class registrationInfo: NSObject {
+class registrationInfo: NSObject  {
     
     var name = String()
     var advisoryNum = Int()
     var lunchPeriod = Int()
     var idNum = Int()
     var school = String()
-    var profilePic = Data()
+    var profilePic = UIImage()
+    var barcodePic = UIImage()
     
 }
